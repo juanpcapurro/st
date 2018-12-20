f07ab60d4bc99f8a815d951306f3669b13da37b3
Juan Pablo Capurro <juanpcapurro@gmail.com>
2018-12-19 21:32:07 -0300

    something similar to terminator's dark mode working

--- config.def.h
+++ config.def.h
@@ -107,8 +107,9 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	"#2e2e2d",
+	"#f2f2f2",
+	"#25C3DC",
 };
 
 
@@ -116,10 +117,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 
 /*
  * Default shape of cursor
